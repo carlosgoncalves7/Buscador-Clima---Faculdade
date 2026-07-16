0e7850d96af6d4f87105c39bf826856d96739178
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-07-16 17:15:55 -0300

    refactor: ajustar a função Hero para melhorar a legibilidade e corrigir a estrutura do HTML

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,26 +1,28 @@
-export default function Hero(hero) {
+export function Hero(hero) {
     const weathericons = {
-        0: "sol.svg",
-        1: "nuvem.svg",
-        2: "nuvem.svg",
-        3: "nuvem.svg",
-        45: "nuvem.svg",
-        51: "chuva.svg",
-        61: "chuva.svg",
-        80: "chuva.svg",
-        95: "trovoada.svg",
-        71: "neve.svg"
+        0: "sol.svg",1: "nuvem.svg",2: "nuvem.svg",3: "nuvem.svg",45: "nuvem.svg",
+        51: "chuva.svg",61: "chuva.svg",80: "chuva.svg",95: "trovoada.svg",71: "neve.svg"
     };
 
+    const cidadeContainer = document.querySelector(".cidade-container")
 
-    return `<div class="cidade-content">
+    cidadeContainer.innerHTML = `<div class="cidade-content">
                 <div class="text-content">
                 <h2 class="cidade">${hero.city.name}</h2>
                 <span class="data">${hero.time.value}</span>
                 </div>
                 <div class="flex-content">
                 <img src="src/assets/light-icon/${weathericons[hero.weathercode]}" alt="" class="vector-clima"></img>
-                <span class="temperatura">${hero.temperature.value}<span class="units">°C</span></span>
+                <p class="temperatura">${hero.temperature.value}<sup>°C</sup></p>
                 </div
-            </div>`
+            </div>`;
+    return
+}
+
+export function CardsHero(details) {
+    return `
+        <div class="card">
+            <p class="titulo">${details.name}</p>
+            <span class="valor">${details.value}${details.units}</span>
+        </div>`
 }
